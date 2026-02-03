c0b13d9689023db6c7c96c859ca0fd2f114007a2
hollow-phantom <hollow.phantom728@gmail.com>
2026-02-03 23:06:30 +0000

    style Button

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -15,31 +15,30 @@ const Index = (): JSX.Element => {
 
             {/* コンテンツ */}
             <View>
-
-                <View>
+                <View style={styles.memoListItem}>
                     <View>
-                        <Text>買い物リスト</Text>
-                        <Text>2023年10月1日 10:00</Text>
+                        <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+                        <Text style={styles.memoListItemDate}>2023年10月1日 10:00</Text>
                     </View>
                     <View>
                         <Text>X</Text>
                     </View>
                 </View>
 
-                <View>
+                <View style={styles.memoListItem}>
                     <View>
-                        <Text>買い物リスト</Text>
-                        <Text>2023年10月1日 10:00</Text>
+                        <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+                        <Text style={styles.memoListItemDate}>2023年10月1日 10:00</Text>
                     </View>
                     <View>
                         <Text>X</Text>
                     </View>
                 </View>
 
-                <View>
+                <View style={styles.memoListItem}>
                     <View>
-                        <Text>買い物リスト</Text>
-                        <Text>2023年10月1日 10:00</Text>
+                        <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+                        <Text style={styles.memoListItemDate}>2023年10月1日 10:00</Text>
                     </View>
                     <View>
                         <Text>X</Text>
@@ -48,14 +47,17 @@ const Index = (): JSX.Element => {
 
             </View>
 
-            <View>
-                <Text>+</Text>
+            <View style={styles.circleButton}>
+                <Text style={styles.circleButtonLabel}>+</Text>
             </View>
         </View>
 
     )
 }
 
+//
+// CSS
+//
 const styles = StyleSheet.create({
     container: {
         flex: 1,
@@ -87,7 +89,56 @@ const styles = StyleSheet.create({
         lineHeight: 32,
         fontWeight: 'bold',
         color: '#ffffff'
-    }
+    },
+
+    // コンテンツ
+    memoListItem: {
+        backgroundColor: '#ffffff',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingVertical: 16,
+        paddingHorizontal: 19,
+        alignItems: 'center',
+        borderBottomWidth: 1,
+        borderColor: 'rgba(0,0,0,0.15)'
+    },
+
+    memoListItemTitle: {
+        fontSize: 16,
+        lineHeight: 32,
+    },
+
+    memoListItemDate: {
+        fontSize: 12,
+        lineHeight: 16,
+        color: '#848484'
+    },
+
+    // ボタン
+    circleButton: {
+        width: 64,
+        height: 64,
+        borderRadius: '50%',
+        backgroundColor: '#467FD3',
+        justifyContent: 'center',
+        alignItems: 'center',
+        position: 'absolute',
+        right:40,
+        bottom: 40,
+        // ドロップシャドウ。iOSにしか効かない
+        shadowColor: '#000000',
+        shadowOpacity: 0.25,
+        shadowRadius: 8,
+        shadowOffset: { width: 0, height: 8 },
+        // Androidでドロップシャドウさせるために必要
+        elevation: 8,
+    },
+
+    circleButtonLabel: {
+        color: '#ffffff',
+        fontSize: 40,
+        lineHeight: 40,
+    },
 
 })
 
