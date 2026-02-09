d7f5f5170e02af47376894f8b9181283bf2893f1
hollow-phantom <hollow.phantom728@gmail.com>
2026-02-09 23:06:36 +0000

    Create Components

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,55 +1,27 @@
 import { JSX } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 
+// 各パーツのコンポーネント
+import Header from '../components/Header'
+import MemoListItem from '../components/MemoListItem'
+import CircleButton from '../components/CircleButton'
+
 const Index = (): JSX.Element => {
 
     return (
         <View style={styles.container}>
             {/* ヘッダー */}
-            <View style={styles.header}>
-                <View style={styles.headerInner}>
-                    <Text style={styles.headerTitle}>Memo App</Text>
-                    <Text style={styles.headerRight}>ログアウト</Text>
-                </View>
-            </View>
+            < Header />
 
             {/* コンテンツ */}
             <View>
-                <View style={styles.memoListItem}>
-                    <View>
-                        <Text style={styles.memoListItemTitle}>買い物リスト</Text>
-                        <Text style={styles.memoListItemDate}>2023年10月1日 10:00</Text>
-                    </View>
-                    <View>
-                        <Text>X</Text>
-                    </View>
-                </View>
-
-                <View style={styles.memoListItem}>
-                    <View>
-                        <Text style={styles.memoListItemTitle}>買い物リスト</Text>
-                        <Text style={styles.memoListItemDate}>2023年10月1日 10:00</Text>
-                    </View>
-                    <View>
-                        <Text>X</Text>
-                    </View>
-                </View>
-
-                <View style={styles.memoListItem}>
-                    <View>
-                        <Text style={styles.memoListItemTitle}>買い物リスト</Text>
-                        <Text style={styles.memoListItemDate}>2023年10月1日 10:00</Text>
-                    </View>
-                    <View>
-                        <Text>X</Text>
-                    </View>
-                </View>
-
+                < MemoListItem />
+                < MemoListItem />
+                < MemoListItem />
             </View>
 
-            <View style={styles.circleButton}>
-                <Text style={styles.circleButtonLabel}>+</Text>
-            </View>
+            {/* ボタン */}
+            < CircleButton>+</CircleButton>
         </View>
 
     )
@@ -65,81 +37,6 @@ const styles = StyleSheet.create({
         // justifyContent: 'center',
         // alignItems: 'center'
     },
-
-    header: {
-        backgroundColor: '#467FD3',
-        height: 104,
-        justifyContent: 'flex-end',
-    },
-
-    headerInner: {
-        alignItems: 'center'
-    },
-
-    headerRight: {
-        position: 'absolute',
-        right: 16,
-        bottom: 16,
-        color: 'rgba(255,255,255,0.7)'
-    },
-
-    headerTitle: {
-        marginBottom: 8,
-        fontSize: 22,
-        lineHeight: 32,
-        fontWeight: 'bold',
-        color: '#ffffff'
-    },
-
-    // コンテンツ
-    memoListItem: {
-        backgroundColor: '#ffffff',
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        paddingVertical: 16,
-        paddingHorizontal: 19,
-        alignItems: 'center',
-        borderBottomWidth: 1,
-        borderColor: 'rgba(0,0,0,0.15)'
-    },
-
-    memoListItemTitle: {
-        fontSize: 16,
-        lineHeight: 32,
-    },
-
-    memoListItemDate: {
-        fontSize: 12,
-        lineHeight: 16,
-        color: '#848484'
-    },
-
-    // ボタン
-    circleButton: {
-        width: 64,
-        height: 64,
-        borderRadius: '50%',
-        backgroundColor: '#467FD3',
-        justifyContent: 'center',
-        alignItems: 'center',
-        position: 'absolute',
-        right:40,
-        bottom: 40,
-        // ドロップシャドウ。iOSにしか効かない
-        shadowColor: '#000000',
-        shadowOpacity: 0.25,
-        shadowRadius: 8,
-        shadowOffset: { width: 0, height: 8 },
-        // Androidでドロップシャドウさせるために必要
-        elevation: 8,
-    },
-
-    circleButtonLabel: {
-        color: '#ffffff',
-        fontSize: 40,
-        lineHeight: 40,
-    },
-
 })
 
 
